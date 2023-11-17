2029d6b385f87568687030ca51db8f8e0779eed1
mamun323064 <mdabdullahal752@gmail.com>
2023-11-17 10:34:23 +0600

    new merry

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+$(function () {
+    /*--------------------------
+	 Global Variable
+	---------------------------- */
+    var patsala = $(window);
+    var page = $('html, body');
+
+    $(window).on('load', function () {
+        $('.preloader').delay(1500).fadeOut(500);
+
+    });
+
+    //     $('.venobox').venobox();
+
+});
